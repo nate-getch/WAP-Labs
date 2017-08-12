56c6a048f65c068ae57216676f781c614c5224da
Natnael getachew <natwinet@gmail.com>
2017-08-12 17:36:15 -0500

    updated the puzzle movement functionality

--- puzzle/fifteen.js
+++ puzzle/fifteen.js
@@ -35,6 +35,8 @@ $(document).ready(function(){
            // $(this).x = x;
           //  $(this).y = y;
         });
+        $('#puzzlearea').append("<div class='whitespot' style='left:300px;top:300px;'></div>");
+        checkWin();
     }
 
     shuffle();
@@ -45,13 +47,69 @@ $(document).ready(function(){
         }
     );
 
-    $("#puzzlearea div").click(function(){
-        alert($(this).css("top"));
-    });
+    $("#puzzlearea div").click(move);
 
     /*
     if($("div").next("div").length > 0 ){
     	alert("hello");
+    }
+    */
+    function move(){
+        //alert($(this).css("top") + " Left:"+ $(this).css("left"));
+        var top = parseInt($(this).css("top"));
+        var left = parseInt($(this).css("left"));
+        var current = $(this);
+
+        var rightElt = $('#puzzlearea div').filter(function () { 
+            return  $(this).css('left') ==  left+100+'px' && $(this).css('top') ==  top+'px';
+        });
+        var leftElt = $('#puzzlearea div').filter(function () { 
+            return  $(this).css('left') ==  left-100+'px' && $(this).css('top') ==  top+'px';
+        });
+        var topElt = $('#puzzlearea div').filter(function () { 
+            return  $(this).css('top') ==  top-100+'px' && $(this).css('left') ==  left+'px';
+        });
+        var bottomElt = $('#puzzlearea div').filter(function () { 
+            return  $(this).css('top') ==  top+100+'px' && $(this).css('left') ==  left+'px';
+        });
+        
+        //check right
+        if(rightElt.hasClass("whitespot") ){
+            current.css({'left':left+100+'px'}).delay(400);
+            $(".whitespot").css({'left':left+'px'});
+        }
+        else if(leftElt.hasClass("whitespot") ){
+            current.css({'left':left-100+'px'});
+            $(".whitespot").css({'left':left+'px'});
+        }
+        else if(topElt.hasClass("whitespot")){
+            current.css({'top':top-100+'px'});
+            $(".whitespot").css({'top':top+'px'});
+        }
+        else if(bottomElt.hasClass("whitespot")){
+            current.css({'top':top+100+'px'});
+            $(".whitespot").css({'top':top+'px'});
+        }
+        checkWin();
+
+        
+    }
+    /*
+    function checkWin(){
+        var win = false;
+
+        $.each( $("#puzzlearea div"), function( i, val ) {
+            var x = ((i % 4) * 100) ;
+            var y = (Math.floor(i / 4) * 100) ;
+            if( !$(this).hasClass("whitespot") && $(this).css("backgroundPosition") != -x + 'px ' + (-y) + 'px' ){
+                win = true;
+                //return false;
+                return;
+            }
+            win = false;
+            
+        });
+        alert(win);
     }
     */
 });
